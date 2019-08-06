a1ff99a583ce016472bb83a4f7d7fa6c412223dc
Marc Roig Vilamala <marc.roig.vilamala@gmail.com>
2019-08-06 15:15:39 +0100

    Improved graph marking where events are detected

--- graph.py
+++ graph.py
@@ -1,13 +1,13 @@
-import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import patches
 
 
 class Graph(object):
     def __init__(self, graph_x_size, expected_events):
         self.graph_x_size = graph_x_size
 
-        x = np.linspace(0, graph_x_size, graph_x_size)
-        y = np.linspace(0, 1, graph_x_size)
+        x = [0, graph_x_size]
+        y = [0, 1]
 
         # You probably won't need this if you're embedding things in a tkinter plot...
         plt.ion()
@@ -16,10 +16,30 @@ class Graph(object):
         self.ax = self.fig.add_subplot(1, 1, 1)
 
         self.lines = {}
+        self.colors = {}
         for event in expected_events:
             self.lines[event], = self.ax.plot(x, y, label=event)  # Returns a tuple of line objects, thus the comma
 
+            self.colors[event] = self.lines[event].get_color()
+
+        self.previous_x_data_length = 0
+
+        self.last_rectangle = {}
+
         plt.legend()
+
+    def add_rectangle(self, event, x1, x2):
+        return self.ax.add_patch(patches.Rectangle((x1, -1), x2 - x1, 3, color=self.colors[event], alpha=0.3))
+
+    def mark_rectangle_until(self, event, x_data, i):
+        if i > 0:
+            last_rectangle = self.last_rectangle.get(event)
+            if last_rectangle and last_rectangle.get_x() + last_rectangle.get_width() == x_data[i - 1]:
+                last_rectangle.set_width(
+                    last_rectangle.get_width() + (x_data[i] - x_data[i - 1])
+                )
+            else:
+                self.last_rectangle[event] = self.add_rectangle(event, x_data[i - 1], x_data[i])
 
     def update(self, evaluation):
         # We can get the x_data from just one of the lines since they should all be the same
@@ -42,7 +62,11 @@ class Graph(object):
             line.set_xdata(x_data)
             line.set_ydata(y_data)
 
+            for i in range(self.previous_x_data_length, len(x_data)):
+                if y_data[i] > 0.01:
+                    self.mark_rectangle_until(event, x_data, i)
+
+        self.previous_x_data_length = len(x_data)
+
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
-
-
