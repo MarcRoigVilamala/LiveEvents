4f542a71cb78639de0d9a2b201add1a220193a3b
Marc Roig Vilamala <marc.roig.vilamala@gmail.com>
2019-08-08 14:02:24 +0100

    Revert "Tmp commit"
    This reverts commit 0db69335e9b4020472e0258b467b3f564a21aede.

--- videoFeed/videoFeed.py
+++ videoFeed/videoFeed.py
@@ -1,31 +1,27 @@
 from itertools import cycle
-from os import listdir
 
 import cv2
 
 
 class VideoFeed(object):
-    def __init__(self, video_name='Fighting006'):
-        self.frames = [
-            cv2.imread('frames/{}/{}'.format(video_name, frame))
-            for frame in sorted(listdir('frames/{}'.format(video_name)))
-        ]
+    def __init__(self, video_file='/home/marc/Videos/UCF_CRIME/Crime/Abuse/Abuse001_x264.mp4'):
+        # self.frames = [
+        #     open('frames/frame0.jpg'.format(frame), 'rb').read()
+        #     for frame in range(100)
+        # ]
 
-        self.current_frame = -1
-
-        self.aux = 0
+        # self.current_frame = -1
 
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
         # self.video = cv2.VideoCapture(0)
-        # self.video = cv2.VideoCapture(video_file)
+        self.video = cv2.VideoCapture(video_file)
 
-    # def __del__(self):
-    #     self.video.release()
+    def __del__(self):
+        self.video.release()
 
     def __iter__(self):
-        return self
         # return cycle(self.frames)
         # return self.frames.__iter__()
 
@@ -34,24 +30,10 @@ class VideoFeed(object):
         #
         # return self.frames[self.current_frame]
 
-        # success, image = self.video.read()
-        # while success:
-        #     yield image
-        #
-        #     success, image = self.video.read()
-        #
-        # self.video.release()
+        success, image = self.video.read()
+        while success:
+            yield image
 
-    def __next__(self):
-        self.current_frame += 1
+            success, image = self.video.read()
 
-        if self.aux < 10:
-            if self.current_frame >= 16:
-                self.aux += 1
-
-            self.current_frame %= 16
-
-        if len(self.frames) > self.current_frame:
-            return self.current_frame, self.frames[self.current_frame]
-        else:
-            raise StopIteration()
+        self.video.release()
