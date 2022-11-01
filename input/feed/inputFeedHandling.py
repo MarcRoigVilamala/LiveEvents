47abfd6aab332a4111090c3b344567490d48117d
Marc Roig Vilamala <marc.roig.vilamala@gmail.com>
2022-11-01 11:20:24 +0000

    Updated inputFeedHandling.py and added redditFeed

--- input/feed/inputFeedHandling.py
+++ input/feed/inputFeedHandling.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def create_video_feed(conf):
+def create_video_feed(conf, input_feed_conf):
     print(
         'Using a VideoFeed as an input may not work correctly at the moment (to be fixed)',
         file=sys.stderr
@@ -9,11 +9,11 @@ def create_video_feed(conf):
     from input.feed.videoFeed import VideoFeed
 
     loop_at = conf['input'].get('loop_at')
-    video_name = conf['input'].get('video_name')
-    if conf['input'].get('video_name') is None:
-        video_class = None
-    else:
-        video_class = video_name
+    video_name = input_feed_conf.get('video_name')
+    if video_name is None:
+        raise ValueError('--video_name needs to be defined when using VideoFeed as the input feed')
+
+    video_class = video_name.split('_')[:-3]
 
     return VideoFeed(
         video_file='~/datasets/Video/UCF_CRIME/DemoVideos/{}/{}.mp4'.format(video_class, video_name),
@@ -21,24 +21,29 @@ def create_video_feed(conf):
     )
 
 
-def create_audio_feed(conf):
+def create_audio_feed(conf, input_feed_conf):
     from input.feed.audioFeed import AudioFeed
 
-    audio_file = conf['input'].get('audio_file')
+    audio_file = input_feed_conf.get('audio_file')
     if audio_file is None:
-        raise ValueError("--audio_file needs to be defined when using AudioFeed as the input_feed_type")
+        raise ValueError("--audio_file needs to be defined when using AudioFeed as the input feed")
 
     return AudioFeed(audio_file)
 
 
-def create_live_audio_feed(conf):
+def create_live_audio_feed(conf, input_feed_conf):
     from input.feed.liveAudioFeed import LiveAudioFeed
     if LiveAudioFeed is None:
         raise Exception("LiveAudioFeed cannot be used as it could not be imported. Make sure pyaudio is installed")
     return LiveAudioFeed()
 
 
-def create_demo_audio_feed(conf):
+def create_reddit_feed(conf, input_feed_conf):
+    from input.feed.redditFeed import RedditFeed
+    return RedditFeed(from_file=input_feed_conf['from_file'])
+
+
+def create_demo_audio_feed(conf, input_feed_conf):
     from input.feed.demoAudioFeed import DemoAudioFeed
     return DemoAudioFeed()
 
@@ -47,14 +52,17 @@ input_feeds = {
     'VideoFeed': create_video_feed,
     'AudioFeed': create_audio_feed,
     'LiveAudioFeed': create_live_audio_feed,
+    'RedditFeed': create_reddit_feed,
     # 'DemoAudioFeed': create_demo_audio_feed,
 }
 
 
 def create_input_feed(conf):
-    input_feed_type = conf['input']['input_feed_type']
+    input_feed_conf = conf['input']['input_feed']
+
+    input_feed_type = input_feed_conf['type']
 
     if input_feed_type in input_feeds:
-        return input_feeds[input_feed_type](conf)
+        return input_feeds[input_feed_type](conf, input_feed_conf)
     else:
         raise ValueError("Unexpected value for input_feed_type: {}".format(input_feed_type))
---
+++ input/feed/redditFeed.py
@@ -0,0 +1,33 @@
+import json
+
+import pandas as pd
+from tqdm import tqdm
+
+
+class RedditFeed(object):
+    def __init__(self, from_file):
+        with open(from_file, 'r') as f:
+            loaded_json = json.load(f)
+
+        loaded_json['post_id'] = loaded_json.pop('_id')
+
+        json_keys = loaded_json.keys()
+
+        columns = [k for k in json_keys]
+
+        entry_list = []
+        for i, identifier in tqdm(loaded_json['post_id'].items()):
+            entry_list.append(
+                [
+                    loaded_json[c][i]  # For each column, get the corresponding value
+                    for c in columns
+                ]
+            )
+
+        self.df = pd.DataFrame(data=entry_list, columns=columns)
+
+    def get_max_length(self):
+        return len(self.df.index)
+
+    def __iter__(self):
+        return self.df.iterrows()
