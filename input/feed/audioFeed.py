c09cc3fea90c46e8c547f6a16cf7a4910c133a0a
Marc Roig Vilamala <marc.roig.vilamala@gmail.com>
2022-11-01 11:19:33 +0000

    Updated audioFeed.py

--- input/feed/audioFeed.py
+++ input/feed/audioFeed.py
@@ -5,7 +5,9 @@ class AudioFeed(object):
     def __init__(self, audio_file):
         vggish_model = torch.hub.load('harritaylor/torchvggish', 'vggish')
 
-        self.processed_file = vggish_model.forward(audio_file)
+        self.audio_file = audio_file
+
+        self.processed_file = vggish_model.forward(self.audio_file)
 
     def get_max_length(self):
         return self.processed_file.shape[0]
